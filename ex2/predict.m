1f0a755b3c5fe3b09d6efcc62e87d79bb3dccf4c
staraple <staraple@gmail.com>
2017-09-28 00:53:12 +0800

    feat(ex2): 完成predict 函数

--- ex2/predict.m
+++ ex2/predict.m
@@ -15,6 +15,15 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+W = (theta' * X')';
+
+for i = 1 : m
+  if W(i) >= 0
+    p(i) = 1;
+  else
+    p(i) = 0;
+  end
+end
 
 
 
